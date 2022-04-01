c1ae2fbf9d7ef45502d34b71c60e4144432d53ce
alinesantiago <aline.alves.santiago@gmail.com>
2022-04-01 16:10:36 -0300

    m

--- js/index.js
+++ js/index.js
@@ -8,13 +8,16 @@ if (cadastroCru != null) {
 
 ////////////////////////////////////////////////////////////////
 
+
 function enviarDados(e) {
+    e.preventDefault();
     var cadastroCru = localStorage.getItem('cadastro')
     if (cadastroCru != null) {
         var cadastro = JSON.parse(cadastroCru);
     } else {
         var cadastro = [];
     }
+    console.log(e.target.elements['digito'].value)
 } //tentar método do video
 
 
@@ -23,26 +26,30 @@ subirDados.addEventListener("click", enviarDados)
 
 
 //Excluindo dados
-function limparDados(e) {
-    //seleciona todas as linhas a serem excluídas
-    linhasExistentes = [...document.querySelectorAll('info-extrato, tbody, extrato-resultado, conteudo-dinamico')];
-    linhasExistentes.forEach((element) => {
-        //remove as linhas
-        element.remove()
-    });
-   window.confirm("Deseja excluir os dados?");
-    //limpa local storage
-    localStorage.clear();
-    var cadastro = [];
-    desenhaCadastro();
-}
+function limparDados() {
+    if (cadastro > 0 && window.confirm("Deseja excluir os dados?") ) {
+        //seleciona todas as linhas a serem excluídas
+        linhasExistentes = [...document.querySelectorAll('info-extrato, tbody, extrato-resultado, conteudo-dinamico')];
+        linhasExistentes.forEach((element) => {
+            //remove as linhas
+            element.remove()
+        });
+        //limpa local storage
+        localStorage.clear();
+        var cadastro = [];
+    } else if (cadastro == 0) {
+        alert('oi')
+    }
+}  //pensei em usar lenght mas dá erro...
+
+
 
 
 //função desenhar cadastro
 function desenhaCadastro() {
     for (central in cadastro) {
-        document.querySelector('table.info-extrato tbody').innerHTML += 
-        `
+        document.querySelector('table.info-extrato tbody').innerHTML +=
+            `
     <tr class="conteudo-dinamico">
         <td class="digito" > ${cadastro[central].digito ? '+' : '-'} </td>
         <td class="mercadoria"> ${cadastro[central].mercadoria} </td>
@@ -53,9 +60,15 @@ function desenhaCadastro() {
     }
 }
 
-if (document.querySelector('info-extrato, tbody, extrato-resultado, conteudo-dinamico') != null){
-    
+//verifica se o cadastro está vazio, e informa.
+if (cadastro.length == 0) {
+    document.querySelector('table.info-extrato tbody.extrato-resultado').innerHTML = `<div> Nenhuma transação cadastrada.</div>`;
 }
+
+var valor = 125.45;
+//com R$
+var dinheiro = numero.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+console.log(dinheiro);
 /////////////////////////////////////////////////////////////////////////////////////////
 
 desenhaCadastro()
